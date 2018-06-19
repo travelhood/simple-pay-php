c68cb593501c4750be65afd5c87d951346741f70
lazos <lajos.bencz@gmail.com>
2018-06-19 15:43:20 +0200

    psr conform file logger

---
+++ src/Travelhood/OtpSimplePay/Logger/FileLogger.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Travelhood\OtpSimplePay\Logger;
+
+use RuntimeException;
+use Psr\Log\AbstractLogger;
+
+class FileLogger extends AbstractLogger
+{
+    const LEVELS = ['debug', 'info', 'notice', 'warning', 'error', 'critical', 'alert', 'emergency'];
+
+    protected $_path;
+    protected $_level;
+    protected $_handle;
+
+    public function __construct($path, $level='debug')
+    {
+        if(!is_file($path)) {
+            touch($path);
+            chmod($path, 0666);
+        }
+        $this->_path = $path;
+        $this->_level = array_search(strtolower($level), self::LEVELS);
+        $this->_handle = fopen($path, 'a+');
+        if(!is_resource($this->_handle)) {
+            throw new RuntimeException('Failed to open handle for logging to file: '.$path);
+        }
+    }
+
+    public function __destruct()
+    {
+        if(is_resource($this->_handle)) {
+            flock($this->_handle, LOCK_UN);
+            fclose($this->_handle);
+        }
+    }
+
+    public function log($level, $message, array $context = [])
+    {
+        $idx = array_search(strtolower($level), self::LEVELS);
+        if($idx < $this->_level) {
+            return;
+        }
+        if(!is_resource($this->_handle)) {
+            throw new RuntimeException('No open handle to log to');
+        }
+        $formatted = '['.date('Y-m-d H:i:s').'] ['.$level.'] '.$message;
+        if(count($context) > 0) {
+            $formatted.= PHP_EOL.json_encode($context, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        }
+        flock($this->_handle, LOCK_EX);
+        $put = fputs($this->_handle, trim($formatted).PHP_EOL);
+        flock($this->_handle, LOCK_UN);
+        if(!$put) {
+            throw new RuntimeException('Failed to write to log file');
+        }
+    }
+}
