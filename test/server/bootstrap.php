2ad3d954f9d2bf3bbd58c45168d06dfadf63e4dd
lazos <lajos.bencz@gmail.com>
2018-06-19 11:59:31 +0200

    merge config

--- test/server/bootstrap.php
+++ test/server/bootstrap.php
@@ -5,4 +5,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 global $simplePay;
 $config = require __DIR__ . '/../fixture/config.php';
 $config = new \Travelhood\OtpSimplePay\Config($config);
+if(is_readable(__DIR__ . '/../fixture/config.local.php')) {
+    $config->mergeConfig(require __DIR__ . '/../fixture/config.local.php');
+}
 $simplePay = new \Travelhood\OtpSimplePay\Service($config);
